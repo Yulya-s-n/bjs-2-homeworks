963896ca42cdffaa6a05fec03f04ae9f324da55f
yuliya <yulya.s.n@bk.ru>
2023-09-08 12:24:09 +0300

    updated 1.base-concepts/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,39 +1,61 @@
-/* Задача1 */
+// Задача1
 
 "use strict"
-  function solveEquation(a, b, c) {
-  	let arr = [];
-  	let discriminant = Math.pow(b, 2) - 4 * a * c;
 
-  	if (discriminant < 0) {
-  		return arr;
-  	} else if (discriminant === 0) {
-  		let answer = -b / (2 * a);
-  		arr.push(answer);
-  	} else if {
-  		let firstAnswer = (-b + Math.sqrt(disc)) / (2 * a);
-  		let secondAnswer = (-b - Math.sqrt(disc)) / (2 * a);
-  		arr.push(firstAnswer, secondAnswer);
-  	}
-  	return arr;
-  }
+function solveEquation(a, b, c) {
+	let arr = [];
+	let d = (b ** 2 - 4 * a * c);
+
+	if (d < 0) {
+	return arr;
+	}
+
+	if (d > 0) {
+	   let firstAnswer = (-b + Math.sqrt(d)) / (2 * a);
+    	let secondAnswer = (-b - Math.sqrt(d)) / (2 * a);
+    	arr.push(firstAnswer, secondAnswer);
+    	console.log(arr);
+		return arr;
+		}
+
+    if (d === 0) {
+		let answer = -b / (2 * a);
+		arr.push(answer);
+		console.log(arr);
+		return arr;
+		}
+		return arr;
+	}
 
 
 
-/* Задача2 */
 
-  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
-    return false;
-  }
+// Задача2
 
-  percent = percent/100/12;
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+// Проверяем аргументы на корректность и преобразуем их в числа
+percent = parseFloat(percent);
+contribution = parseFloat(contribution);
+amount = parseFloat(amount);
+countMonths = parseInt(countMonths);
 
-  let loanBody = amount - contribution;
-  let monthlyPayment = loanBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
-  let totalPayment = monthlyPayment * countMonths;
+// Проверяем, являются ли аргументы числами после преобразования
+if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+	return false;
+}
 
-  totalPayment = Number(totalPayment.toFixed(2));
+// Преобразуем процентную ставку из годовой в месячную и долю первоначального взноса
+var monthlyInterestRate = percent / 100 / 12;
+var loanBody = amount - contribution;
 
-  return totalPayment;
+// Рассчитываем ежемесячный платеж
+var monthlyPayment = loanBody * (monthlyInterestRate + monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, countMonths) - 1));
+
+// Рассчитываем общую сумму, которую клиент должен вернуть банку
+var totalAmount = monthlyPayment * countMonths;
+
+// Округляем результат до двух знаков после запятой
+totalAmount = parseFloat(totalAmount.toFixed(2));
+
+return totalAmount;
 }
